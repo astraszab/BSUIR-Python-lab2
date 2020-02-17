d1add0f6338a96a71227df5dca1b4bf8346d49fd
astraszab <astarshab1@gmail.com>
2020-02-17 11:54:23 +0300

    Add test for json

---
+++ lab2_package/test_json.py
@@ -0,0 +1,46 @@
+from lab2_package.json import dumps
+import pytest
+
+
+def test_none():
+    assert dumps(None) == 'null'
+
+
+def test_number():
+    assert dumps(123) == '123'
+
+
+def test_string():
+    assert dumps('string') == '"string"'
+
+
+def test_bool():
+    assert dumps(True) == 'true'
+
+
+def test_list():
+    assert dumps([1, 2, True, None]) == '[1, 2, true, null]'
+
+
+def test_dict():
+    assert dumps({1: 'a', 'abc': None}) == '{"1": "a", "abc": null}'
+
+
+def test_nested_dict():
+    assert dumps({5: {'a': 3}, 'b': {'c': 4}}) == \
+        '{"5": {"a": 3}, "b": {"c": 4}}'
+
+
+def test_wrong_type():
+    with pytest.raises(TypeError):
+        dumps(...)
+
+
+def test_infinity():
+    with pytest.raises(ValueError):
+        dumps(float('inf'))
+
+
+def test_nan():
+    with pytest.raises(ValueError):
+        dumps(float('nan'))
