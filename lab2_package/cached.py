e39c08e95f663d17cdbf851235fc4f051a1248c0
astraszab <astarshab1@gmail.com>
2020-02-17 17:54:10 +0300

    Add cached

---
+++ lab2_package/cached.py
@@ -0,0 +1,21 @@
+import functools
+import warnings
+
+
+def cached(func):
+    @functools.wraps(func)
+    def caching_wrapper(*args, **kwargs):
+        try:
+            if (args, tuple(kwargs.items())) in cache:
+                return cache[args, tuple(kwargs.items())]
+            else:
+                res = func(*args, **kwargs)
+                cache[args, tuple(kwargs.items())] = res
+                return res
+        except TypeError:
+            warnings.warn(
+                "arguments cannot be hashed so caching is impossible.",
+                RuntimeWarning)
+            return func(*args, **kwargs)
+    cache = {}
+    return caching_wrapper
