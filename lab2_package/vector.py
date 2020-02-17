f75b94501b6b8d814012df722cead69cf2c0bfbc
astraszab <astarshab1@gmail.com>
2020-02-17 15:51:28 +0300

    Bug fixes for vector

--- lab2_package/vector.py
+++ lab2_package/vector.py
@@ -1,5 +1,7 @@
 class Vector:
-    def __init__(self, values):
+    def __init__(self, values=None):
+        if values is None:
+            values = []
         for value in values:
             if not isinstance(value, (int, float)):
                 raise ValueError('Vector should be numerical, '
@@ -7,7 +9,10 @@ class Vector:
         self._vector = values
 
     def __eq__(self, other):
-        return self._vector == other._vector
+        if isinstance(other, Vector):
+            return self._vector == other._vector
+        else:
+            return False
 
     def __getitem__(self, key):
         return self._vector[key]
@@ -27,17 +32,41 @@ class Vector:
 
     def __add__(self, other):
         if isinstance(other, Vector):
+            if len(self) != len(other):
+                raise ValueError('Vectors should have same length to '
+                                 'perform an operation.')
             return Vector([self[i] + other[i] for i in range(len(self))])
         elif isinstance(other, (int, float)):
             return Vector([value + other for value in self])
+        else:
+            raise ValueError('Input for the operation should be numerical, '
+                             f'{type(other).__name__} provided.')
+
+    def __radd__(self, other):
+        return self + other
 
     def __sub__(self, other):
+        if not isinstance(other, (Vector, int, float)):
+            raise ValueError('Input for the operation should be numerical, '
+                             f'{type(other).__name__} provided.')
         return self + (-other)
 
+    def __rsub__(self, other):
+        return -(self - other)
+
     def __mul__(self, other):
+        if not isinstance(other, (int, float)):
+            raise ValueError('Input for the operation should be a number, '
+                             f'{type(other).__name__} provided.')
         return Vector([value * other for value in self])
 
+    def __rmul__(self, other):
+        return self * other
+
     def __matmul__(self, other):
+        if not isinstance(other, Vector):
+            raise ValueError('Input for the operation should be a vector, '
+                             f'{type(other).__name__} provided.')
         if len(self) != len(other):
             raise ValueError('Vectors should have same length to '
                              'perform an operation.')
