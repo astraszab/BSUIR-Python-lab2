4d4da322707c4116a9de2b78712c011051cf4ce7
astraszab <astarshab1@gmail.com>
2020-02-20 09:41:14 +0300

    Change package name

--- lab2_package/setup.py
+++ lab2_package/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="lab2-atraszab",
+    name="lab2_astraszab",
     version="0.0.1",
     author="Uladzislau Astraszab",
     author_email="u.astraszab@gmail.com",
