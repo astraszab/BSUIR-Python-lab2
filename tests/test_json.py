23aac9ad4b5a01e975373a259189e8890b05a861
astraszab <astarshab1@gmail.com>
2020-02-25 12:08:34 +0300

    Use standard json module in json tests

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,34 +1,39 @@
 from lab2.src.json_serializer import dumps
+import json
 import pytest
 
 
 def test_none():
-    assert dumps(None) == 'null'
+    assert dumps(None) == json.dumps(None)
 
 
 def test_number():
-    assert dumps(123) == '123'
+    assert dumps(123) == json.dumps(123)
 
 
 def test_string():
-    assert dumps('string') == '"string"'
+    assert dumps('string') == json.dumps('string')
 
 
 def test_bool():
-    assert dumps(True) == 'true'
+    assert dumps(True) == json.dumps(True)
 
 
 def test_list():
-    assert dumps([1, 2, True, None]) == '[1, 2, true, null]'
+    assert dumps([1, 2, True, None]) == json.dumps([1, 2, True, None])
 
 
 def test_dict():
-    assert dumps({1: 'a', 'abc': None}) == '{"1": "a", "abc": null}'
+    assert dumps({1: 'a', 'abc': None}) == json.dumps({1: 'a', 'abc': None})
 
 
 def test_nested_dict():
-    assert dumps({5: {'a': 3}, 'b': {'c': 4}}) == \
-        '{"5": {"a": 3}, "b": {"c": 4}}'
+    obj = {5: {'a': 3},
+           'b': {'c': {3: 5}},
+           True: False,
+           None: [1, 2, 3, 4, 5],}
+    assert dumps(obj) == \
+        json.dumps(obj)
 
 
 def test_wrong_type():
